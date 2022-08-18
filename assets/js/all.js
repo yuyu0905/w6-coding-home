5180a84f58903b76b54b5367e345eb891ec00b5e
amy <amy>
2022-08-19 02:21:56 +0800

    Update 2022-08-18T18:21:52.831Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,37 @@
 "use strict";
 
 $(function () {
-  var swiper = new Swiper(".swiper", {
+  var swiper = new Swiper(".swiper-program", {
+    slidesPerView: 3,
+    spaceBetween: 8,
+    //多欄
+    breakpoints: {
+      768: {
+        slidesPerView: 6,
+        spaceBetween: 8
+      },
+      992: {
+        slidesPerView: 9,
+        spaceBetween: 16
+      }
+    },
+    speed: 1500,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 0
+    }
+  });
+  var shareSwiper = new Swiper(".share-swiper", {
     // Optional parameters
     effect: "fade",
+    loop: true,
     fadeEffect: {
       crossFade: true
     },
     // 分頁
     pagination: {
-      el: ".swiper-pagination"
+      el: ".share-swiper-pagination",
+      clickable: true
     },
     // 左右箭頭
     navigation: {
