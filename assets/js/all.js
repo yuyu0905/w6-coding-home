fdeabf26e7306d5993ec96b412af4d467b301249
amy <amy>
2022-08-18 17:28:34 +0800

    Update 2022-08-18T09:28:30.890Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,21 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  var swiper = new Swiper(".swiper", {
+    // Optional parameters
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    // 左右箭頭
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
