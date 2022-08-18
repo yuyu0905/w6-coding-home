d96b5d875ff27e29dcbebc855ef30c487af6ec8e
amy <amy>
2022-08-19 02:18:58 +0800

    修改首頁輪播 & 套用字體樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,13 +1,39 @@
 $(function() {
-  const swiper = new Swiper(".swiper", {
+  var swiper = new Swiper(".swiper-program", {
+    slidesPerView: 3,
+    spaceBetween: 8,
+    //多欄
+    breakpoints: {
+        768: {
+            slidesPerView: 6,
+            spaceBetween: 8,
+        },
+        992: {
+            slidesPerView: 9,
+            spaceBetween: 16,
+            
+        }
+    },
+    speed: 1500,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 0
+    },
+  });
+
+
+
+  const shareSwiper = new Swiper(".share-swiper", {
     // Optional parameters
     effect: "fade",
+    loop: true,
     fadeEffect: {
       crossFade: true
     },
     // 分頁
     pagination: {
-      el: ".swiper-pagination"
+      el: ".share-swiper-pagination",
+      clickable: true
     },
     // 左右箭頭
     navigation: {
