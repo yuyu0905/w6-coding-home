d939d3f68ca8d398a709426fb694caf291302d10
amy <amy>
2022-08-18 17:27:54 +0800

    首頁輪播+課程介紹完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,19 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    // 左右箭頭
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+  
 });
